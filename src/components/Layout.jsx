70263332bdb2a20727a0b31215081c27772c8092
SeniruR <seniruranasinghe@gmail.com>
2025-06-23 16:30:39 +0530

    changes added.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 import Navigation from "./pages/Navigation";
 import Sidebar from "./pages/Sidebar";
 
 const Layout = ({ children }) => {
   const [open, setOpen] = useState(false);
   const toggleDrawer = () => setOpen(!open);
+  const location = useLocation();
+
+  const isLoginPage = location.pathname === "/login";
 
   return (
-    <div className="flex flex-col min-h-screen font-sans">
+    <div className="flex flex-col min-h-screen font-sans w-full" style={{ width: '100vw' }}>
       <Navigation />
       <div className="flex flex-col mt-20 h-[calc(100vh-80px)]">
-        <div className="flex-1 p-8 bg-gray-100 flex">
+        <div className="flex-1 p-8 bg-gray-100 flex" style={{ backgroundColor: 'white' }}>
           <Sidebar open={open} toggleDrawer={toggleDrawer} />
-          <div className="mt-[64px] m-[10px] w-full">{children}</div>
+          {isLoginPage ? (
+            <div className="w-full" style={{ color: 'black' }}>
+              {children}
+            </div>
+          ) : (
+            <div className="mt-[64px] w-full" style={{ color: 'black' }}>
+              {children}
+            </div>
+          )}
         </div>
       </div>
     </div>
