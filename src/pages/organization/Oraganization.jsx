6886a1e0687718720998e24d480adb454b379082
Himasha Chinthani <140165425+HimashaChinthani@users.noreply.github.com>
2025-10-20 22:47:39 +0530

    commit

--- src/pages/organization/Oraganization.jsx
+++ src/pages/organization/Oraganization.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Dashboard from "./Dashboard"; // matches Dashboard.jsx
+import Dashboard from './DashBoard';
 import FarmerDetailsList from './FarmerDetailsList';
 import FarmerDetails from './FarmerDetails';
 
